93d23cd4d47136ce3e74bc9099bd0f41caa90e18
AugustGao <August.Gao@mspbots.ai>
2025-02-21 12:25:41 +0800

    main

--- packages/core/plot.ts
+++ packages/core/plot.ts
@@ -10,7 +10,7 @@ export abstract class Plot extends EE {
 
   public constructor(container: string | HTMLElement) {
     super()
-    this.container = typeof container === 'string' ? document.getElementById(container)! : container;
+    this.container = typeof container === 'string' ? document.getElementById(container) : container;
 
     this.createG2()
   }
@@ -21,12 +21,42 @@ export abstract class Plot extends EE {
 
   private getChartOptions() {
     return {
-      container: this.container,
+      container: this.container
+    }
+  }
+
+  private getSpecOptions() {
+    return {
+      type: "interval",
+      autoFit: true,
+      data: [
+        { name: "London", 月份: "Jan.", 月均降雨量: 18.9 },
+        { name: "London", 月份: "Feb.", 月均降雨量: 28.8 },
+        { name: "London", 月份: "Mar.", 月均降雨量: 39.3 },
+        { name: "London", 月份: "Apr.", 月均降雨量: 81.4 },
+        { name: "London", 月份: "May", 月均降雨量: 47 },
+        { name: "London", 月份: "Jun.", 月均降雨量: 20.3 },
+        { name: "London", 月份: "Jul.", 月均降雨量: 24 },
+        { name: "London", 月份: "Aug.", 月均降雨量: 35.6 },
+        { name: "Berlin", 月份: "Jan.", 月均降雨量: 12.4 },
+        { name: "Berlin", 月份: "Feb.", 月均降雨量: 23.2 },
+        { name: "Berlin", 月份: "Mar.", 月均降雨量: 34.5 },
+        { name: "Berlin", 月份: "Apr.", 月均降雨量: 99.7 },
+        { name: "Berlin", 月份: "May", 月均降雨量: 52.6 },
+        { name: "Berlin", 月份: "Jun.", 月均降雨量: 35.5 },
+        { name: "Berlin", 月份: "Jul.", 月均降雨量: 37.4 },
+        { name: "Berlin", 月份: "Aug.", 月均降雨量: 42.4 },
+      ],
+      encode: { x: "月份", y: "月均降雨量", color: "name" },
+      transform: [{ type: "stackY" }],
+      interaction: { elementHighlight: { background: true } },
     }
   }
 
   public render() {
+    this.chart.options(this.getSpecOptions() as any);
 
+    this.chart.render()
   }
 
   public destroy() {
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { Bar } from "../packages/plot"
+
+
+onMounted(() => {
+  const bar = new Bar("bar")
+
+  bar.render()
+})
 </script>
 
 <template>
-  <div>
- 
-  </div>
+  <div id="bar" style="width:100vw;height:100vh;"></div>
 </template>
 
+<style>
+
+</style>
