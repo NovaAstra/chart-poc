76b242ca4c7cb1a246449168c4cbf9893c284914
NovaAstra <NovaAstra@outlook.com>
2025-02-23 21:08:02 +0800

    main

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,4 +1,7 @@
-import type { Chart as G2Chart, ChartOptions } from "@antv/g2"
+import type {
+  Chart,
+  ChartOptions
+} from "@antv/g2"
 
 import EE from '@antv/event-emitter';
 import { pick } from "lodash-es"
@@ -9,34 +12,38 @@ export const CHART_OPTIONS = ['renderer'] as const;
 
 const SOURCE_ATTRIBUTE_NAME = 'data-chart-source-type';
 
-export const Chart = extend(Runtime, { ...stdlib(), ...plotlib() });
+export const G2Chart = extend(Runtime, { ...stdlib(), ...plotlib() });
 
 export type Options = {
   container: string | HTMLElement;
-  adaptor?: Adaptor<Options>;
 }
+
+const override = (options: Options) => options
 
 export abstract class Adaptor<O extends Options> {
   public abstract applyOptions(plot: Plot<O>): O;
 }
 
 export abstract class Plot<O extends Options> extends EE {
+  public readonly options: O;
+
   public readonly container: HTMLElement;
 
-  public chart: G2Chart;
+  public readonly adaptor: Adaptor<O>;
+
+  public chart: Chart;
 
   private createG2() {
     if (!this.container) {
       throw Error('The container is not initialized!');
     }
 
-    this.chart = new Chart(this.getChartOptions())
+    this.chart = new G2Chart(this.getChartOptions())
     this.container.setAttribute(SOURCE_ATTRIBUTE_NAME, 'Sphere Charts');
   }
 
-  public render(): G2Chart {
-    const { adaptor } = this.options
-    const options = adaptor.applyOptions(this)
+  public render(): Chart {
+    const options = this.adaptor.applyOptions(this)
 
     this.chart.options(options)
     this.chart.render()
@@ -44,8 +51,10 @@ export abstract class Plot<O extends Options> extends EE {
     return this.chart;
   }
 
-  public destroy() {
+  public destroy(): Chart {
     this.chart?.destroy()
+
+    return this.chart;
   }
 
   private getChartOptions() {
@@ -53,11 +62,15 @@ export abstract class Plot<O extends Options> extends EE {
     return options as ChartOptions
   }
 
-  public constructor(public readonly options: O) {
+  protected abstract getDefaultOptions(): Partial<Options>;
+
+  public constructor(options: O, adaptor?: Adaptor<O>) {
     super()
 
     const { container } = options
     this.container = typeof container === 'string' ? document.getElementById(container) : container;
+    this.options = override(options) as O
+    this.adaptor = adaptor
 
     this.createG2()
   }
